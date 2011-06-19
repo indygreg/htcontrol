a1113f8ce80bb7f96bf0fd5338f6b5cef4166d5f
Gregory Szorc <gregory.szorc@gmail.com>
2011-06-18 22:42:29 -0700

    add SpeechControl and HTControl projects
    marks introduction of Kinect speech recognition!

---
+++ HTControl/HTControl.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace HTControl {
+    public partial class HTControl : Form {
+        protected SpeechControl.ComponentControl SpeechComponentControl;
+        protected Thread SpeechThread;
+
+        protected SerialControl.PioneerTv TV;
+
+        public HTControl() {
+            InitializeComponent();
+
+            this.SpeechThread = new Thread(() =>
+            {
+                this.SpeechComponentControl = new SpeechControl.ComponentControl();
+                this.SpeechComponentControl.CommandReceived +=new SpeechControl.ComponentControl.SpeechCommandReceivedHandler(ProcessSpeechCommand);
+            });
+            this.SpeechThread.Start();
+
+            this.TV = new SerialControl.PioneerTv("COM3");
+        }
+
+        protected void ProcessSpeechCommand(SpeechControl.ComponentControl.SpeechCommand command) {
+            switch(command) {
+                case SpeechControl.ComponentControl.SpeechCommand.TelevisionOff:
+                    this.TV.PowerOff();
+                    break;
+
+                case SpeechControl.ComponentControl.SpeechCommand.TelevisionOn:
+                    this.TV.PowerOn();
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown speech command received");
+
+                    break;
+            }
+        }
+    }
+}
---
+++ SpeechControl/ComponentControl.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Research.Kinect.Audio;
+using Microsoft.Speech.AudioFormat;
+using Microsoft.Speech.Recognition;
+using System.IO;
+
+namespace SpeechControl
+{
+    /// <summary>
+    /// Speech control driver for home theater
+    /// </summary>
+    public class ComponentControl : IDisposable
+    {
+        private const string RecognizerId = "SR_MS_en-US_Kinect_10.0";
+
+        protected KinectAudioSource AudioSource;
+        protected RecognizerInfo Recognizer;
+        protected SpeechRecognitionEngine Engine;
+        protected Stream AudioStream;
+
+        public enum SpeechCommand {
+            TelevisionOn,
+            TelevisionOff,
+        }
+
+        public delegate void SpeechCommandReceivedHandler(SpeechCommand command);
+
+        /// <summary>
+        /// Fired when a command is received
+        /// </summary>
+        public event SpeechCommandReceivedHandler CommandReceived;
+
+        public ComponentControl()
+        {
+            this.AudioSource = new KinectAudioSource();
+
+            this.AudioSource.FeatureMode = true;
+            this.AudioSource.AutomaticGainControl = false;
+            this.AudioSource.SystemMode = SystemMode.OptibeamArrayAndAec;
+            this.AudioSource.BeamChanged += new EventHandler<BeamChangedEventArgs>(AudioSource_BeamChanged);
+            
+            this.Recognizer = SpeechRecognitionEngine.InstalledRecognizers().Where(r => r.Id == RecognizerId).FirstOrDefault();
+
+            if(this.Recognizer == null) {
+                throw new Exception("Could not find Kinect speech recognizer");
+            }
+
+            this.Engine = new SpeechRecognitionEngine(Recognizer.Id);
+            this.Engine.UnloadAllGrammars();
+
+            this.LoadGrammer();
+
+            this.AudioStream = this.AudioSource.Start();
+            this.Engine.SetInputToAudioStream(this.AudioStream, new SpeechAudioFormatInfo(EncodingFormat.Pcm, 16000, 16, 1,
+                                                      32000, 2, null));
+
+            this.Engine.SpeechHypothesized += new EventHandler<SpeechHypothesizedEventArgs>(Engine_SpeechHypothesized);
+
+            this.Engine.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        void AudioSource_BeamChanged(object sender, BeamChangedEventArgs e)
+        {
+            Console.WriteLine("Beam angle: " + e.Angle);
+        }
+
+        protected void LoadGrammer() {
+            var gb = new GrammarBuilder();
+            gb.Culture = this.Recognizer.Culture;
+
+            gb.Append("turn on television");
+            var g = new Grammar(gb);
+            g.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(TurnOnTelevision);
+            this.Engine.LoadGrammar(g);
+
+            gb = new GrammarBuilder("turn off television");
+            gb.Culture = this.Recognizer.Culture;
+            g = new Grammar(gb);
+            g.SpeechRecognized += TurnOffTelevision;
+            this.Engine.LoadGrammar(g);
+
+        }
+
+        void Engine_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            Console.WriteLine("Speech hypothesized: " + e.Result.Text);
+        }
+
+        void TurnOnTelevision(object sender, SpeechRecognizedEventArgs e) {
+            this.RaiseEvent(e, SpeechCommand.TelevisionOn);
+        }
+
+        void TurnOffTelevision(object sender, SpeechRecognizedEventArgs e) {
+            this.RaiseEvent(e, SpeechCommand.TelevisionOff);
+        }
+
+        protected void RaiseEvent(SpeechRecognizedEventArgs e, SpeechCommand command) {
+            Console.WriteLine(e.Result.Confidence + " " + e.Result.Text);
+
+            if (e.Result.Confidence < 0.9) return;
+
+            var ev = this.CommandReceived;
+            if (ev != null) {
+                ev(command);
+            }
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            if (this.AudioStream != null)
+                this.AudioStream.Dispose();
+
+            if (this.AudioSource != null)
+                this.AudioSource.Dispose();
+
+            if (this.Engine != null)
+                this.Engine.Dispose();
+        }
+
+        #endregion
+    }
+}
